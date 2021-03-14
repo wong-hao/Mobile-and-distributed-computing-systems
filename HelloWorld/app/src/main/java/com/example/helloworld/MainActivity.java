eaba45e181b2cefc36acaf6670c7d130a9fddae1
王昊 <229077035@qq.com>
2021-03-14 17:03:22 +0800

    update

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Run time text: FIT5046");
+        //setContentView(R.layout.activity_main);
+    }
+}
